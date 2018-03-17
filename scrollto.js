847fa551588223d06fc421c1d69540ea328465f2
Dacrol <jonas.ahrens@gmail.com>
2018-03-17 21:21:16 +0100

    Add assignation code

---
+++ scrollto.js
@@ -0,0 +1,53 @@
+// @ts-ignore
+require('jquery-ui/ui/effect');
+
+/**
+ * Assigns the scrollTo method to jQuery.
+ *
+ * @param {JQueryStatic} $
+ */
+function assignScrollTo ($) {
+  if (!$) {
+    throw new Error('$ not found.');
+  }
+
+  const abortScroll = function () {
+    $('html, body').stop();
+    document.removeEventListener('wheel', abortScroll);
+  };
+
+  Object.assign($, {
+    scrollTo: function (
+      target,
+      time = 1500,
+      easing = 'easeInOutCubic',
+      callbackFn = undefined
+    ) {
+      document.addEventListener('wheel', abortScroll);
+      if (typeof time === 'function') {
+        callbackFn = time;
+        time = 1500;
+      }
+      if (typeof easing === 'function') {
+        callbackFn = easing;
+        easing = 'easeInOutCubic';
+      }
+      $('html, body').animate(
+        {
+          scrollTop: $(target).offset().top
+        },
+        time,
+        easing,
+        callbackFn
+      );
+      // console.log('Scrolled');
+    }
+  });
+}
+
+export default (() => {
+  if (typeof $ === 'function') {
+    assignScrollTo($);
+  }
+  return assignScrollTo;
+})();
